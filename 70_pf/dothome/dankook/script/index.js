1654244962e0762de8b1b88a05fb4bf1573cfb97
JunHuyk <145418839+JunHuyk@users.noreply.github.com>
2025-05-29 16:23:34 +0900

    0529

--- 70_pf/dothome/dankook/script/index.js
+++ 70_pf/dothome/dankook/script/index.js
@@ -1,16 +1,16 @@
-const gnb = document.querySelector('.gnb');
-const inner = document.querySelectorAll('.navinner')
+const slidegnb = document.querySelector('.slidegnb');
+const inner = document.querySelectorAll('.navinner');
 let idx = 0;
 
 setInterval(() => {
   idx = (idx + 1) % inner.length;
-  gnb.style.transition = "transform .3s ease";
-  gnb.style.transform = `translateX(-${idx * 100}vw)`;
+  slidegnb.style.transition = "transform .3s ease";
+  slidegnb.style.transform = `translateX(-${idx * 100}vw)`;
   if( idx === inner.length-1){
     setTimeout(()=>{
-        gnb.style.transition = 'none';
+        slidegnb.style.transition = 'none';
         idx=0;
-        gnb.style.transform = `translateX(-${idx*100}vw)`;
+        slidegnb.style.transform = `translateX(-${idx*100}vw)`;
     },300)
     
   }
