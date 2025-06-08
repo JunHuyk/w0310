30277711945f7eec1a5959706566942bc5605300
JunHuyk <wnscjcj2000@naver.com>
2025-06-08 18:33:51 +0900

    0608

--- 연습장/1/index.js
+++ 연습장/1/index.js
@@ -1,17 +1,9 @@
-const slideList = document.querySelector(".slide_list");
 const inner = document.querySelectorAll(".inner");
 
-idx = 0;
+let idx = 0;
 
 setInterval(() => {
-    idx++;
-    slideList.style.transition = "transform .3s ease";
-    slideList.style.transform = `translateY(-${idx*300}px)`;
-    setTimeout(() => {
-        if (idx === inner.length-1){
-        slideList.style.transition = "none";
-        idx = 0;
-        slideList.style.transform = `translateY(-${idx*300}px)`;
-    }        
-    }, 300);
+    inner[idx].classList.remove("active");
+    idx = (idx+1) % inner.length;
+    inner[idx].classList.add("active");
 }, 3000);
