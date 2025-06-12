b3cd0f196459ff7cd470d28d629f83766de43b7c
JunHuyk <wnscjcj2000@naver.com>
2025-06-13 00:24:29 +0900

    0613

--- 연습장/1/0611/index.js
+++ 연습장/1/0611/index.js
@@ -1,17 +1,9 @@
-const slide = document.querySelector(".slidelist");
-const inner = document.querySelectorAll(".inner");
+const inner = document.querySelectorAll('.inner');
 
 let i = 0;
 
 setInterval(() => {
-    i++;
-    slide.style.transition = "transform .3s ease";
-    slide.style.transform = `translateX(-${i*25}%)`;
-    if(i === inner.length-1) {
-        setTimeout(() => {
-            slide.style.transition = 'none';
-            i = 0;
-            slide.style.transform = `translateX(-${i*25}%)`;    
-        }, 500);
-    }
-}, 2500);
+    inner[i].classList.remove('active');
+    i = (i + 1) % inner.length;
+    inner[i].classList.add('active');
+}, 3000);
