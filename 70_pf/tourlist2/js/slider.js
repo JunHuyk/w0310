ab76b2c15e5c2b1327704b3c8bba652c2678510f
JunHuyk <145418839+JunHuyk@users.noreply.github.com>
2025-06-23 16:09:56 +0900

    0623

---
+++ 70_pf/tourlist2/js/slider.js
@@ -0,0 +1,16 @@
+
+
+$(()=>{
+  $('.bxslider').bxSlider({
+    mode:'fade',
+    auto: true,
+    // autoControls: true 
+    stopAutoOnClick: true,
+    //play/pause - 실행 일시정지
+    //pagenation(indicator) -> 하단중앙의 동그라미 버튼
+    pager: true,
+    slideWidth: 1903
+  });
+})
+
+
