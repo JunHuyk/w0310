47a6148f0d3f6140672819db2e3c6b5c4d85c251
JunHuyk <145418839+JunHuyk@users.noreply.github.com>
2025-07-02 16:58:49 +0900

    0702

--- 70_pf/steam/js/mypage.js
+++ 70_pf/steam/js/mypage.js
@@ -1,3 +1,9 @@
 const userName = document.querySelector(".username");
+const editBtn = document.querySelector(".edit-btn");
 const user = localStorage.getItem("loginUser");
+
 userName.textContent = user;
+
+editBtn.addEventListener("click", () => {
+  window.location.href = "/w0310/70_pf/steam/sub/profile_edit.html";
+});
