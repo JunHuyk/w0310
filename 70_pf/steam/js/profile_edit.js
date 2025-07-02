a6074b6b0a05cd356a37bc4081327f563c476746
JunHuyk <145418839+JunHuyk@users.noreply.github.com>
2025-07-02 17:41:28 +0900

    0702

--- 70_pf/steam/js/profile_edit.js
+++ 70_pf/steam/js/profile_edit.js
@@ -3,7 +3,7 @@ const saveBtn = document.querySelector(".save-btn");
 
 saveBtn.addEventListener("click", () => {
   const userName = document.getElementById("username").value;
-  const passWord = document.getElementById("password").value;
+  const passWord = document.getElementById("new_password").value;
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const tel = document.getElementById("tel").value;
@@ -17,4 +17,5 @@ saveBtn.addEventListener("click", () => {
   };
 
   localStorage.setItem("signUpData", JSON.stringify(updateData));
+  localStorage.setItem("loginUser", updateData.userName);
 });
